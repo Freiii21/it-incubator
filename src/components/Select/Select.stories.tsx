f73ec7712b80247f317261184b207dd6cca7a57f
Freiii21 <mrfreiii@gmail.com>
2021-11-10 23:06:34 +0300

    #42 lesson_15_4

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,6 +1,7 @@
 import {Select, SelectPropsType} from './Select'
 import { Story } from '@storybook/react';
 import {action} from '@storybook/addon-actions';
+import {useState} from 'react';
 
 export default {
     title: 'components/Select',
@@ -29,5 +30,37 @@ WithoutValue.args = {
         {title:"Canada",value:2},
         {title:"USA",value:3},
         {title:"Japan",value:4}
+    ]
+}
+
+const Template2: Story<SelectPropsType> = (args) => {
+    const [value, setValue] = useState("2")
+
+    return <Select {...args} value={value} onChange={setValue}/>
+}
+
+export const WithValueAndUseState = Template2.bind({})
+WithValueAndUseState.args = {
+    items: [
+        {title:"Australia",value:"1"},
+        {title:"Canada",value:"2"},
+        {title:"USA",value:"3"},
+        {title:"Japan",value:"4"}
+    ]
+}
+
+const Template3: Story<SelectPropsType> = (args) => {
+    const [value, setValue] = useState(null)
+
+    return <Select {...args} value={value} onChange={setValue}/>
+}
+
+export const WithoutValueAndUseState = Template3.bind({})
+WithoutValueAndUseState.args = {
+    items: [
+        {title:"Australia",value:"1"},
+        {title:"Canada",value:"2"},
+        {title:"USA",value:"3"},
+        {title:"Japan",value:"4"}
     ]
 }
