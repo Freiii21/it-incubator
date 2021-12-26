e7b04c54e2bb7b09da07d9a7529c60af805e0fcf
Freiii21 <mrfreiii@gmail.com>
2021-12-26 21:03:17 +0300

    #42 lesson_22

---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react'
+
+type PropsType = {
+}
+
+const get2DigitsString = (number: number) => number < 10 ? "0"+number : number;
+
+export const Clock: React.FC<PropsType> = (props) => {
+    const [date, setDate] = useState(new Date())
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date())
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId)
+        }
+    }, [])
+
+
+    return (
+        <div>
+            <span>{get2DigitsString(date.getHours())}</span>
+            :
+            <span>{get2DigitsString(date.getMinutes())}</span>
+            :
+            <span>{get2DigitsString(date.getSeconds())}</span>
+        </div>
+    )
+}
